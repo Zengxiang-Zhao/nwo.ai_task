69f35cbc57bc755899c9f31a9d12ee6cb3be3682
Zengxiang Zhao <zhaozx0308@gmail.com>
2021-11-11 22:11:25 -0500

    add extract_corpora.py to extract corpora from tables tweet and reddit

---
+++ extract_corpora.py
@@ -0,0 +1,65 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import pandas as pd
+from optparse import OptionParser
+
+# connect to the database
+credentials = service_account.Credentials.from_service_account_file(
+'credential.json')
+project_id = "nwo-sample"
+client = bigquery.Client(credentials= credentials,project=project_id)
+
+def corpora(keyword,limit):
+    """
+    extract corpora from graph.tweets and graph.reddit associated with keyword
+    args:
+        keyword: string
+        limit: int or None
+    rtype:
+        list of strings
+    """
+    query_reddit_all_corpora = f"""
+        SELECT body as corpora FROM graph.reddit
+        WHERE body like '% {keyword} %'
+        """
+    query_tweet_all_corpora = f"""
+        SELECT tweet as corpora FROM graph.tweets
+        WHERE tweet like '% {keyword} %'
+        """
+
+    if limit:
+        query_reddit_all_corpora += f'\nLIMIT {limit}'
+        query_tweet_all_corpora += f'\nLIMIT {limit}'
+
+    reddit_all_corpora = (
+        client.query(query_reddit_all_corpora)
+        .result()
+        .to_dataframe()
+        )
+
+    tweet_all_corpora = (
+        client.query(query_tweet_all_corpora)
+        .result()
+        .to_dataframe()
+        )
+
+    keyword_corpora = pd.concat([reddit_all_corpora,tweet_all_corpora])
+
+    return keyword_corpora['corpora'].values
+
+
+if __name__ == "__main__":
+    optparser = OptionParser()
+    optparser.add_option('-k', '--keyword',
+                         dest='keyword',
+                         help='keyword',
+                         default=None)
+    optparser.add_option('-l', '--limit',
+                         dest='limit',
+                         help='limit the number of output in corpora',
+                         default=None,
+                         type='int')
+    (options, args) = optparser.parse_args()
+
+    corpora = corpora(options.keyword, options.limit)
+    print(corpora)
